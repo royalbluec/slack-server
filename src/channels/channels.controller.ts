7838f291009e5e1a1b87cb4b13c66f34a1149fa3
royalbluee <tkyeom96@gmail.com>
2022-02-20 23:40:44 +0900

    feat: Add other api

---
+++ src/channels/channels.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { query } from 'express';
+
+@Controller('api/workspaces/:url/channels')
+export class ChannelsController {
+  @Get()
+  getAllChannels() {}
+
+  @Post()
+  createChannel() {}
+
+  @Get(':name')
+  getSpecificChannel() {}
+
+  @Get(':name/chats')
+  getChat(@Query() query, @Param() param) {
+    console.log(query.perPage, query.page);
+    console.log(param.id, param.url);
+  }
+
+  @Post(':name/chats')
+  postChat(@Body() body) {}
+
+  @Get(':name/members')
+  getAllMembers() {}
+
+  @Post(':name/members')
+  inviteMembers() {}
+}
---
+++ src/workspaces/workspaces.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('api/workspaces')
+export class WorkspacesController {
+  @Get()
+  getMyWorkspaces() {}
+
+  @Post()
+  createWorkspace() {}
+
+  @Get(':url/members')
+  getAllMembersFromWorkspace() {}
+
+  @Post(':url/members')
+  inviteMembersFromWorkspace() {}
+}
